5ba8b887a1ce8cb1a12e93ffb8e92370b4fbfb88
Alexandria P <alxpettit@gmail.com>
2022-04-24 15:53:52 -0700

    whoops

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 
 target=$(basename "$0")
 echo "Target: ${target}"
-execute=$(fd "${target}.sh" -t x "${HOME}/.local/share/JetBrains/Toolbox/apps/" | tail -n 1)
+execute=$(fd "${target}\.sh" -t x "${HOME}/.local/share/JetBrains/Toolbox/apps/" | tail -n 1)
 echo "Executing: ${execute} $@"
 exec "${execute}" "$@"
 
