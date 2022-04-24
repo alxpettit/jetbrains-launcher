97488aef46d52d00802c3fa5eac0d60627ea56bc
Alexandria P <alxpettit@gmail.com>
2022-04-24 15:51:18 -0700

    uwu

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@
 mkdir -p /opt/jetbrains-launcher/
 sudo cp -v *.sh /opt/jetbrains-launcher/
 sudo chown root:root /opt/jetbrains-launcher/*.sh
-sudo chmod 655 /opt/jetbrains-launcher/*.sh
+sudo chmod 755 /opt/jetbrains-launcher/*.sh
+
 names=( studio pycharm idea clion rubymine webstorm phpstorm goland rider datagrip dataspell )
+
 for name in ${names[@]}; do
     echo "Installing: ${name}"
     sudo ln -sv "/opt/jetbrains-launcher/launcher.sh" "/usr/local/bin/${name}" -f
